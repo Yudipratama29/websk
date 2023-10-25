67e3ad08317388c272428a77764c90567188eaf2
Yudipratama29 <pratamayudi2001@gmail.com>
2023-10-25 19:47:30 +0800

    update data MK

--- app/Http/Controllers/MKController.php
+++ app/Http/Controllers/MKController.php
@@ -6,12 +6,32 @@ use Illuminate\Http\Request;
 
 class MKController extends Controller
 {
+    private $mk = [
+        [
+            'ID' => 'SK01',
+            'Nama' => 'Object Oriented Programming',
+            'Jurusan' => 'Sistem Komputer'
+        ],
+        [
+            'ID' => 'SK02',
+            'Nama' => 'Web Programming',
+            'Jurusan' => 'Sistem Komputer' 
+        ],
+        [
+            'ID' => 'SK03',
+            'Nama' => 'Sensor and Transduser',
+            'Jurusan' => 'Sistem Komputer'
+        ],
+    ];
+
     public function index()
     {
+        return view('mk.index', ['data' => $this->mk] );
     }
 
     public function create()
     {
+        return view('mk.create');
     }
 
     public function show($id)
