c64558c9e064253d4f7e3b5d31004a056ad8b8f9
Yudipratama29 <p.yudi2001@gmail.com>
2023-11-22 19:47:54 +0800

    Using Eloquent ORM Laravel

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,72 +4,96 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\mahasiswa;
+use App\Models\jurusan;
 
 class MahasiswaController extends Controller
 {
     public function index()
     {
-        $mhs = DB::table('mhs')
-        ->select("mhs.id","nim", "mhs.nama", "jurusan_id", "jurusan.nama AS jurusan_nama")
-        ->join('jurusan', 'jurusan.id', '=', 'mhs.jurusan_id')
-        ->get();
+        // $mhs = DB::table('mhs')
+        // ->select("mhs.id","nim", "mhs.nama", "jurusan_id", "jurusan.nama AS jurusan_nama")
+        // ->join('jurusan', 'jurusan.id', '=', 'mhs.jurusan_id')
+        // ->get();
+
+        $mhs = Mahasiswa::with('jurusan')->get();
 
         return view('mahasiswa.index', ['data' => $mhs]);
     }
 
     public function create()
     {
-        $jurusan = DB::table('jurusan')->get();
+        // $jurusan = DB::table('jurusan')->get();
+        $jurusan = Jurusan::all();
 
         return view('mahasiswa.create', ['jurusan' => $jurusan]);
     }
 
     public function store(Request $request)
     {
-        DB::table('mhs')->insert([
+        $mhs = Mahasiswa::create([
             'nim' => $request->nim,
             'nama' => $request->nama,
             'jurusan_id' => $request->jurusan,
         ]);
 
-        return redirect(url('/mahasiswa'));
+            return redirect(url('/mahasiswa'));
 
     }
-    public function update(Request $request, $id)
+    public function update(Request $request, Mahasiswa $mhs)
     {
-        DB::table('mhs')
-        ->where('id', $id)
-        ->update([
+        // $mhs = Mahasiswa::find($id);
+
+        $mhs->update([
             'nim' => $request->nim,
             'nama' => $request->nama,
             'jurusan_id' => $request->jurusan,
         ]);
 
+        // DB::table('mhs')
+        // ->where('id', $id)
+        // ->update([
+        //     'nim' => $request->nim,
+        //     'nama' => $request->nama,
+        //     'jurusan_id' => $request->jurusan,
+        // ]);
+
+        // $mhs-save()
+
         return redirect(url('/mahasiswa'));
 
     }
 
-    public function destroy( $id)
+    public function destroy(Mahasiswa $mhs)
     {
-        DB::table('mhs')
-        ->where('id', $id)
-        ->delete();
+        // DB::table('mhs')
+        // ->where('id', $id)
+        // ->delete();
+
+        // $mhs = Mahasiswa::find($id);
+
+        $mhs->delete();
 
         return redirect(url('/mahasiswa'));
 
     }
 
-    public function edit($id)
+    public function edit(Mahasiswa $mhs)
     {
-        $mhs = DB::table('mhs')
-        ->select("mhs.id","nim", "mhs.nama", "jurusan_id", "jurusan.nama AS jurusan_nama")
-        ->join('jurusan', 'jurusan.id', '=', 'mhs.jurusan_id')
-        ->where('mhs.id', $id)
-        ->first();
+        // $mhs = DB::table('mhs')
+        // ->select("mhs.id","nim", "mhs.nama", "jurusan_id", "jurusan.nama AS jurusan_nama")
+        // ->join('jurusan', 'jurusan.id', '=', 'mhs.jurusan_id')
+        // ->where('mhs.id', $id)
+        // ->first();
 
-        $jurusan = DB::table('jurusan')->get();
 
-        return view('mahasiswa.edit', ['data' => $mhs, 'id' => $id, 'jurusan' => $jurusan]);
+        // $jurusan = DB::table('jurusan')->get();
+
+        // $mhs = Mahasiswa::find($id);
+
+        $jurusan = Jurusan::all();
+
+        return view('mahasiswa.edit', ['data' => $mhs, 'id' => $mhs->id, 'jurusan' => $jurusan]);
     }
 
     public function show($id)
